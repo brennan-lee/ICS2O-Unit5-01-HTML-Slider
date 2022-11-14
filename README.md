bc1d350ceeb6b7611c8e14e374737ac96287d35b
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-14 14:25:15 -0500

    added slider and changed names for scope

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICS2O-PWA-Template-HTML
+# ICS2O-Unit5-01-HTML-Slider
 
-[![Mr Coxall's Super Linter](https://github.com/brennan-lee/ICS2O-PWA-Test/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/brennan-lee/ICS2O-PWA-Test/actions)
+[![Mr Coxall's Super Linter](https://github.com/brennan-lee/ICS2O-Unit5-01-HTML-Slider/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/brennan-lee/ICS2O-Unit5-01-HTML-Slider/actions)
 
-[![Run on Repl.it](https://repl.it/badge/github/brennan-lee/ICS2O-PWA-Test)](https://repl.it/github/brennan-lee/ICS2O-PWA-Test)
+[![Run on Repl.it](https://repl.it/badge/github/brennan-lee/ICS2O-Unit5-01-HTML-Slider)](https://repl.it/github/brennan-lee/ICS2O-Unit5-01-HTML-Slider)
 
-This site can be found at: [https://brennan-lee.github.io/ICS2O-PWA-Test/](https://brennan-lee.github.io/ICS2O-PWA-Test/)
+This site can be found at: [https://brennan-lee.github.io/ICS2O-Unit5-01-HTML-Slider/](https://brennan-lee.github.io/ICS2O-Unit5-01-HTML-Slider/)
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan Lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan Lee
+// Created on: nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,24 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML-Slider/sw.js", {
+    scope: "/ICS2O-Unit5-01-HTML-Slider/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function updates the slider value.
  */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function displays the slider value.
+ */
+
+
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
 }
